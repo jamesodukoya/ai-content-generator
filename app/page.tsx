760cc07e7e4dfa8095b71dd810d8a94db2c953cc
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-07-19 12:40:38 +0100

    Initial commit with authentication set up

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Generate content using next-gen AI capabilities</h2>
+      <Button>Start Generating</Button>
+    </div>
+  );
+}
